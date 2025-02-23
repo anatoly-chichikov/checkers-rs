bad2b8517ec68d64f7f2ee94ee6d3e1b9f9c3a9a
a.chichikov <a.chichikov@semrush.com>
2025-02-23 20:16:56 +0200

    feat: Add colored board display with improved visual feedback

--- src/ui.rs
+++ src/ui.rs
@@ -1,11 +1,13 @@
 use crossterm::{
     cursor,
+    style::{Color, ResetColor, SetForegroundColor},
     terminal::{Clear, ClearType},
     QueueableCommand,
 };
 use std::io::{self, stdout, Write};
 
 use crate::game::CheckersGame;
+use crate::piece::Color as PieceColor;
 
 pub struct UI {
     cursor_pos: (usize, usize),
@@ -33,49 +35,73 @@ impl UI {
         stdout.queue(Clear(ClearType::All))?;
         stdout.queue(cursor::MoveTo(0, 0))?;
 
-        // Print column headers
+        // Print column headers with blue color
+        stdout.queue(SetForegroundColor(Color::Blue))?;
         stdout.write_all(b"   ")?;  // Extra space for alignment
         for col in 0..game.board.size {
             write!(stdout, " {} ", (b'A' + col as u8) as char)?;
         }
+        stdout.queue(ResetColor)?;
         stdout.write_all(b"\n\r")?;
 
         // Print board with row numbers
         for row in 0..game.board.size {
+            stdout.queue(SetForegroundColor(Color::Blue))?;
             write!(stdout, "{:2} ", row + 1)?;
+            stdout.queue(ResetColor)?;
+            
             for col in 0..game.board.size {
                 let is_cursor_here = (row, col) == self.cursor_pos;
                 let is_selected = game.selected_piece == Some((row, col));
 
-                let cell = match game.board.get_piece(row, col) {
-                    Some(piece) => piece.display(),
-                    None => '.',
+                let (piece_char, piece_color) = match game.board.get_piece(row, col) {
+                    Some(piece) => (piece.display(), match piece.color {
+                        PieceColor::White => Color::White,
+                        PieceColor::Black => Color::Red,
+                    }),
+                    None => ('.', Color::DarkGrey),
                 };
 
                 if is_cursor_here {
-                    write!(stdout, "[{}]", cell)?;
+                    stdout.queue(SetForegroundColor(Color::Yellow))?;
+                    write!(stdout, "[{}]", piece_char)?;
                 } else if is_selected {
-                    write!(stdout, "({0})", cell)?;
+                    stdout.queue(SetForegroundColor(Color::Green))?;
+                    write!(stdout, "({0})", piece_char)?;
                 } else {
-                    write!(stdout, " {0} ", cell)?;
+                    stdout.queue(SetForegroundColor(piece_color))?;
+                    write!(stdout, " {0} ", piece_char)?;
                 }
+                stdout.queue(ResetColor)?;
             }
             stdout.write_all(b"\n\r")?;
         }
 
         // Print game status
         stdout.write_all(b"\n\rCurrent player: ")?;
+        let player_color = match game.current_player {
+            PieceColor::White => Color::White,
+            PieceColor::Black => Color::Red,
+        };
+        stdout.queue(SetForegroundColor(player_color))?;
         writeln!(stdout, "{:?}", game.current_player)?;
+        stdout.queue(ResetColor)?;
         
         if let Some(selected) = game.selected_piece {
+            stdout.queue(SetForegroundColor(Color::Green))?;
             writeln!(stdout, "Selected piece at: {}", Self::format_position(selected))?;
+            stdout.queue(ResetColor)?;
         }
 
         if game.is_game_over {
             if let Some(winner) = game.check_winner() {
+                stdout.queue(SetForegroundColor(Color::Yellow))?;
                 writeln!(stdout, "\nGame Over! {:?} wins!", winner)?;
+                stdout.queue(ResetColor)?;
             } else if game.is_stalemate() {
+                stdout.queue(SetForegroundColor(Color::Yellow))?;
                 writeln!(stdout, "\nGame Over! Stalemate!")?;
+                stdout.queue(ResetColor)?;
             }
         }
 
